9ba6c89b2d9d2dcad9e161f7526a53fd0a02e693
Y.Watanabe <tofu.mental@gmail.com>
2023-12-31 10:44:56 +0900

    up

--- src/app/youtube/page.tsx
+++ src/app/youtube/page.tsx
@@ -50,7 +50,7 @@ export default function Youtube() {
       <header>
         <div className='h-28 bg-red-600 flex items-center justify-center'>
           <div className='sm:w-8/12'>
-            <h1 className='text-4xl text-white'>けんぴ。ちゃんテナ</h1>
+            <h1 className='sm:text-4xl text-3xl text-white'>けんぴ。ちゃんテナ</h1>
             <p className='pt-2 pl-2 text-gray-800'>from Youtube</p>
           </div>
         </div>
