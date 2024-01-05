67be8927bf1efb40accc2910e9ef1fdacae72385
Y.W <130629956+Y-Watanabe-Eng@users.noreply.github.com>
2024-01-05 15:43:45 +0900

    next.config.js の更新

--- next.config.js
+++ next.config.js
@@ -2,6 +2,21 @@
 const nextConfig = {
     images: {
         domains: ['i.ytimg.com'],
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/about',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'max-age=0, s-maxage=60'
+          },
+        ],
+      },
+    ];
+  },
+
     }
 }
 
